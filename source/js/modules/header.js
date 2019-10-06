e1d8bd71073eb1a0dd989403ec8d3565255cbbc5
Eugenia <janevega25@gmail.com>
2019-10-06 14:53:31 +0300

    правки

--- source/js/modules/header.js
+++ source/js/modules/header.js
@@ -3,6 +3,7 @@
 (function () {
   var navToggle = document.querySelector('.page-header__toggle');
   var header = document.querySelector('.page-header');
+  var main = document.querySelector('.page-main');
 
   if (!navToggle || !header) {
     return;
@@ -11,6 +12,7 @@
   header.classList.remove('page-header--nojs');
   navToggle.addEventListener('click', function() {
   header.classList.toggle('page-header--open');
+  main.classList.toggle('page-main--open');
 
     if (header.classList.contains('page-header--open')) {
       bodyScrollLock.disableBodyScroll();
